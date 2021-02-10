8d32927ae48214ba1fe6817ef0264c0221effd48
SANKY2SIDHU <sanky2sidhu@gmail.com>
2021-02-10 22:56:38 +0530

    some db changes

--- server.php
+++ server.php
@@ -7,7 +7,7 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'CMS');
+$db = mysqli_connect('localhost', 'root', 'Sidhu@27', 'mygms');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
@@ -29,7 +29,7 @@ if (isset($_POST['reg_user'])) {
 
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $user_check_query = "SELECT * FROM users WHERE name='$username' OR mailid='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
@@ -47,7 +47,7 @@ if (isset($_POST['reg_user'])) {
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
 
-  	$query = "INSERT INTO users (username, email, password, phone) 
+  	$query = "INSERT INTO users (name, mailid, password, contact) 
   			  VALUES('$username', '$email', '$password', '$phone')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
@@ -73,7 +73,7 @@ if (isset($_POST['login_user'])) {
 
   if (count($errors) == 0) {
     $password = md5($password);
-    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $query = "SELECT * FROM users WHERE name='$username' AND password='$password'";
     $results = mysqli_query($db, $query);
     if (mysqli_num_rows($results) == 1) {
       $_SESSION['username'] = $username;
